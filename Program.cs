a08363ee6af9c9c975437a2c59a3661e3c0b62fb
JenniferSecib <jennifer.gullung.ext@secib.fr>
2021-12-01 22:49:50 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SonarSweepFirstPart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\j.gullung\source\repos\SonarSweepFirstPart\input.txt");
+            int[] depths = Array.ConvertAll(lines, s => int.Parse(s));
+            int previousDepth = depths[0];
+            int increases = 0;
+
+            for (int i = 0; i < depths.Length; i++)
+            {
+                if (previousDepth < depths[i])
+                {
+                    increases++;
+                }
+                previousDepth = depths[i];
+            }
+            Console.WriteLine(increases);
+        }
+    }
+}
